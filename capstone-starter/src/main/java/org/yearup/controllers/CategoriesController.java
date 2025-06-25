e38c27be8fe912e659f5ef44423c84c3568849b0
Kevin Gagante <KevinGagante@Gmail.com>
2025-06-25 15:41:41 -0700

    created get all products in category and create category endpoints

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -10,54 +10,43 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
 @RestController
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
 @RequestMapping("/categories")
-// add annotation to allow cross site origin requests
 @CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
     @Autowired
     public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
     {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
-    // add the appropriate annotation for a get action
     @GetMapping("")
     @PreAuthorize("permitAll()")
     public List<Category> getAll()
     {
         try
         {
-            // call the categoryDao to find and return all categories
             return categoryDao.getAllCategories();
         }
         catch(Exception e) {
-            // handle any exceptions that may occur
             throw new RuntimeException("Error retrieving categories", e);
         }
     }
 
-    // add the appropriate annotation for a get action
     @GetMapping("/{categoryId}")
     @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id)
     {
-        // get the category by id
         try
         {
             return categoryDao.getById(id);
         }
         catch(Exception e)
         {
-            // handle any exceptions that may occur
             throw new RuntimeException("Error retrieving category with id: " + id, e);
         }
     }
@@ -67,16 +56,30 @@ public class CategoriesController
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
-        // get a list of product by categoryId
-        return null;
+        try
+        {
+            return productDao.listByCategoryId(categoryId);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException("Error retrieving products for category with id: " + categoryId, e);
+        }
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
-        return null;
+        try
+        {
+            return categoryDao.create(category);
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException("Error adding category", e);
+        }
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
