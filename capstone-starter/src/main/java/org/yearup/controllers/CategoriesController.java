c5b5fb646e29d71a3319799c43022b9ca5800f80
Kevin Gagante <KevinGagante@Gmail.com>
2025-06-25 22:42:50 -0700

    built out dao methods for categories + debugged

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,10 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -13,99 +15,73 @@ import java.util.List;
 @RestController
 @RequestMapping("/categories")
 @CrossOrigin
-public class CategoriesController
-{
+public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
     @Autowired
-    public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
-    {
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
+
     @GetMapping("")
     @PreAuthorize("permitAll()")
-    public List<Category> getAll()
-    {
-        try
-        {
+    public List<Category> getAll() {
+        try {
             return categoryDao.getAllCategories();
-        }
-        catch(Exception e) {
-            throw new RuntimeException("Error retrieving categories", e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/{categoryId}")
     @PreAuthorize("permitAll()")
-    public Category getById(@PathVariable int id)
-    {
-        try
-        {
-            return categoryDao.getById(id);
-        }
-        catch(Exception e)
-        {
-            throw new RuntimeException("Error retrieving category with id: " + id, e);
-        }
+    public Category getById(@PathVariable("categoryId") int categoryId) {
+        try {
+            var category = categoryDao.getById(categoryId);
+            if (category == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            return category;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);        }
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        try
-        {
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        try {
             return productDao.listByCategoryId(categoryId);
-        }
-        catch (Exception e)
-        {
-            throw new RuntimeException("Error retrieving products for category with id: " + categoryId, e);
-        }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);        }
     }
 
 
     @PostMapping("")
     @PreAuthorize("hasRole('ADMIN')")
-    public Category addCategory(@RequestBody Category category)
-    {
-        try
-        {
+    public Category addCategory(@RequestBody Category category) {
+        try {
             return categoryDao.create(category);
-        }
-        catch(Exception e)
-        {
-            throw new RuntimeException("Error adding category", e);
-        }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);        }
     }
 
     @PutMapping("/{categoryId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
-        try
-        {
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
+        try {
             categoryDao.update(id, category);
-        }
-        catch(Exception e)
-        {
-            throw new RuntimeException("Error updating category with id: " + id, e);
-        }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);        }
     }
 
     @DeleteMapping("/{categoryId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void deleteCategory(@PathVariable int id)
-    {
-        try
-        {
+    public void deleteCategory(@PathVariable int id) {
+        try {
             categoryDao.delete(id);
-        }
-        catch(Exception e)
-        {
-            throw new RuntimeException("Error deleting category with id: " + id, e);
-        }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);        }
     }
 }
