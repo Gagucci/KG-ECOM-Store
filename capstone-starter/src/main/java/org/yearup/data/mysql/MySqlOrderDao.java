0e821f7dd9a60ca8586ddc2411844ab3ad69cdc1
Kevin Gagante <KevinGagante@Gmail.com>
2025-06-26 22:46:27 -0700

    built out create order method

--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -15,7 +15,31 @@ public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
 
     @Override
     public Order create(Order order) {
-        return null;
+        String sql = "INSERT INTO orders (user_id, date, address, city, state, zip, shipping_amount) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (var connection = getConnection();
+                var ps = connection.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS)) {
+
+                ps.setInt(1, order.getUserId());
+                ps.setObject(2, order.getDate());
+                ps.setString(3, order.getAddress());
+                ps.setString(4, order.getCity());
+                ps.setString(5, order.getState());
+                ps.setString(6, order.getZip());
+                ps.setBigDecimal(7, order.getShippingAmount());
+
+                ps.executeUpdate();
+
+                try (var rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        order.setOrderId(rs.getInt(1));
+                    }
+                }
+
+                return order;
+            } catch (Exception e) {
+                throw new RuntimeException("Error creating order", e);
+            }
     }
 
     @Override
